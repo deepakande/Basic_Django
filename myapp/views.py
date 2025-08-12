c2ab3422d53f3d7e3abf968992ff1a0075a94ca8
Deepa <deepakandel113@gmail.com>
2025-08-12 11:56:53 +0545

    Docker has added

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("Hello World Deepa, welcome to Django! This is the root page.")
 
 def greet(request):
     return HttpResponse("Hello World Deepa,welcome to Django!")
